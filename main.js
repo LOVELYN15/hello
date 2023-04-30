471fd195bea21e92840c651d45482dbf54b57ecf
LOVELYN15 <lovelynokore4@gmail.com>
2023-04-30 22:29:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const helpers = require("./helper");
+const moreHelpers = require("./moreHelpers");
+const evenMoreHelpers = require("./evenMoreHelpers");
+
+helpers.sayHi();
+
+console.log(moreHelpers.firstName);
+moreHelpers.sayHello();
+moreHelpers.sayGoodbye();
+
+evenMoreHelpers(); // this is a function on its own!
